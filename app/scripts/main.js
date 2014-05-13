b4ec96d9d3a7dcc8db330f0d59fdb62850ffad1a
Andy Georgia <andrewgeorgia@Andrews-MacBook-Air.local>
2014-05-13 19:49:03 -0400

    fixedthesaveissue

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,7 +2,47 @@ $(".button1").click(function(){
  $(".example1").css("background","yellow");
  });
 
-$(".button2").click(function(){
-  (".example2").append.text("<strong> Awesome </strong>")
+$(function () {
+  $(".button2").click(function () {
+    $("<p>Awesome</p>").appendTo(".example2");
+  });
+});
+
+$(".button3").click(function() {
+  $(".example3").animate({
+    opacity: 0.25,
+    left: "+=50",
+    height: "toggle"
+  }, 5000, function() {
+
+  });
+});
+
+$(".button4").click(function(){
+  $(".example4 ul").css("color","blue");
+});
+
+$(".button5").click(function() {
+  $(".example5").animate(
+    {opacity: 0},
+    'slow',
+    'swing',
+    function() {
+      alert('404.Server Error');
+    });
+});
+
+$( ".button6" ).click(function() {
+  $( ".example6" ).animate({
+    width: "70%",
+    height: "10%",
+    opacity: 0.5000,
+    marginLeft: "2in",
+    marginRight: "2in",
+  }, 4000 );
+});
+
+$(".button7").click(function() {
+  window.location = 'https://www.facebook.com' + this.id;
 });
 
