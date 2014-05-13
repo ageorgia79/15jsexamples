960877586c4c877c8254f922aa7687b799d6ee22
Andy Georgia <andrewgeorgia@Andrews-MacBook-Air.local>
2014-05-13 15:21:10 -0400

    saveproject

---
+++ app/scripts/main.js
@@ -0,0 +1,8 @@
+$(".button1").click(function(){ 
+ $(".example1").css("background","yellow");
+ });
+
+$(".button2").click(function(){
+  (".example2").append.text("<strong> Awesome </strong>")
+});
+
